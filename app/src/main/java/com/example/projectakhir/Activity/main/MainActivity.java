ffaf07e813edf03f75239efdfd14ddd8d129e98a
rico-enrique <terongraos@gmail.com>
2022-12-17 17:26:48 +0700

    Bug Fixes
    - Menambahkan Test Page untuk mempermudah Front-end Mengecek halaman

--- app/src/main/java/com/example/projectakhir/Activity/main/MainActivity.java
+++ app/src/main/java/com/example/projectakhir/Activity/main/MainActivity.java
@@ -1,53 +1,69 @@
 package com.example.projectakhir.Activity.main;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.projectakhir.Activity.dashboard.DashboardActivity;
+import com.example.projectakhir.Activity.login.LoginActivity;
+import com.example.projectakhir.Activity.sistempenjualan.PilihBarangActivity;
+import com.example.projectakhir.Activity.sistempenjualan.SistemPenjualanAdminActivity;
+import com.example.projectakhir.R;
 
-//    AppCompatActivity btn_success, btn_failed;
-//
-//    //Dialog Variables
-//    AlertDialog.Builder builderDialog;
-//    AlertDialog alertDialog;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        btn_success = (AppCompatActivity)findViewById(R.id.buttonSuccess);
-//        btn_failed = (AppCompatActivity)findViewById(R.id.buttonFailed);
-//
-//        btn_success.setOnClikListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                showAlertDialog(R.layout.my_success_dialog);
-//            }
-//        });
-//
-//        btn_failed.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void setonClick(View view){
-//                showAlertDialog(R.layout.my_failed_dialog);
-//            }
-//        });
-//                private void showAlertDialog(int myLayout){
-//            builderDialog = new AlertDialog.Builder( this);
-//            View layoutView = getLayoutInflater().inflate(myLayout, null);
-//
-//            AppCompatButton dialogButton = layoutView.findViewById(R.id.buttonOk);
-//            builderDialog.setView(layoutView);
-//            alertDialog = builderDialog.create();
-//            alertDialog.show();
-//
-//            //Click on Ok Button
-//            dialogButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    //Dismiss Dialog
-//                    alertDialog.dismiss();
-//                }
-//            });
-//        }
-//    }
+public class MainActivity extends AppCompatActivity {
+    Button btnDashboard, btnLogin, btnPilihBarang, btnSistemPenjualan, btnSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_features);
+
+        btnDashboard        = findViewById(R.id.btnDashboard);
+        btnLogin            = findViewById(R.id.btnLogin);
+        btnPilihBarang      = findViewById(R.id.btnPilihBarang);
+        btnSistemPenjualan  = findViewById(R.id.btnSistemPenjualan);
+        btnSplash           = findViewById(R.id.btnSplash);
+
+        // Mengaktifkan OnClickListener
+        btnDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        btnPilihBarang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Error! Under Maintance", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(MainActivity.this, PilihBarangActivity.class));
+            }
+        });
+
+        btnSistemPenjualan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SistemPenjualanAdminActivity.class));
+            }
+        });
+
+        btnSplash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SplasActivity.class));
+                Toast.makeText(MainActivity.this,"Kosong Mas!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/projectakhir/Activity/sistempenjualan/PilihBarangActivity.java
+++ app/src/main/java/com/example/projectakhir/Activity/sistempenjualan/PilihBarangActivity.java
@@ -1,4 +1,17 @@
 package com.example.projectakhir.Activity.sistempenjualan;
 
-public class PilihBarangActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projectakhir.R;
+
+public class PilihBarangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_barang);
+    }
+
 }
