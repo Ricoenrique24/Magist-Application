bc6052c3d22fdd8fb66a2566af266b493b86b555
rico-enrique <terongraos@gmail.com>
2022-12-17 23:21:04 +0700

    Add Features "Dashboard" in Test Page
    - Merevisi Tampilan Dashboard
    - Menambahkan komponen pendukung untuk tampilan Dashboard (List_Item, dll)

--- app/src/main/java/com/example/projectakhir/Activity/dashboard/DashboardActivity.java
+++ app/src/main/java/com/example/projectakhir/Activity/dashboard/DashboardActivity.java
@@ -11,6 +11,6 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_dashboard_test);
     }
 }
